cd7ff9915938cad1daa69c81bd8dd9cd001453fa
DonAce29 <140861106+DonAce29@users.noreply.github.com>
2024-11-19 02:01:38 -0500

    responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,18 +5,21 @@ import './Header.css';
 const Header = ({ onCartClick, cartCount }) => {
   return (
     <header>
+      
       <h1 className="logo">
         <img src="logo.jpeg" alt="Logo de Opitas Food" className="logo-image" />
-        Opitas Food
+        <h2 id='opitasFood'>Opitas Food</h2>
       </h1>
 
       <nav className="nav">
         {/* <a href="#menu" className="link">Menú</a>
-        <a href="#ubicaciones" className="link">Ubicaciones</a>
-        <a href="#contacto" className="link">Contacto</a>
+    
+        
         <button className="order-button">Pide Ahora</button> */}
+        <a href="#contacto" className="link">Contacto</a>
+         <a href="#ubicaciones" className="link">Ubicacion</a>
         <a href="#carrito" id='carro' className="cart-container" onClick={onCartClick}>
-          🛒 Carrito
+          🛒 
           
           {cartCount > 0 && (
             <span className="cart-count">{cartCount}</span>
