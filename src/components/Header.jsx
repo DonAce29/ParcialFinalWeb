d285193487614a1e7d40d97a6f9b577ac1efe924
NICOLAS DONNEYS ALVARADO <nicolas.donneys@uao.edu.co>
2024-11-19 18:46:55 -0500

    tercero

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Header.css';
+import 'Header.css';
 
 const Header = ({ onCartClick, cartCount }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
