772e9b06f74b53df12eabb59375166644a2a6bad
NICOLAS DONNEYS ALVARADO <nicolas.donneys@uao.edu.co>
2024-11-19 19:05:35 -0500

    error

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import 'Header.css';
+import 'header.css';
 
 const Header = ({ onCartClick, cartCount }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
