c7905dd039029e8af364f205c4e6ef0a3f23fdae
NICOLAS DONNEYS ALVARADO <nicolas.donneys@uao.edu.co>
2024-11-19 19:52:21 -0500

    Suma

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,24 +1,38 @@
-
 import React from 'react';
 import './cart.css';
 
 const Cart = ({ cartItems, onRemove, onClose }) => {
+  const totalPrice = cartItems.reduce((acc, item) => acc + item.price, 0);
+
   return (
     <div className="cart-container">
       {/* Botón de Cerrar */}
-      <button className="close-button" onClick={onClose}>Cerrar</button>
+      <button className="close-button" onClick={onClose}>
+        Cerrar
+      </button>
 
-      <h2 className='Tit'>Carrito</h2>
+      <h2 className="Tit">Carrito</h2>
       {cartItems.length === 0 ? (
         <p>Carrito vacío</p>
       ) : (
-        cartItems.map((item, index) => (
-          <div key={index} className="cart-item">
-            <p>{item.name}</p>
-            <p>${item.price.toFixed(2)}</p>
-            <button onClick={() => onRemove(item)} className="remove-button">Quitar</button>
+        <>
+          {cartItems.map((item, index) => (
+            <div key={index} className="cart-item">
+              <p>{item.name}</p>
+              <p>${item.price.toFixed(2)}</p>
+              <button
+                onClick={() => onRemove(item)}
+                className="remove-button"
+              >
+                Quitar
+              </button>
+            </div>
+          ))}
+          {/* Muestra el total */}
+          <div className="cart-total">
+            <h3>Total: ${totalPrice.toFixed(2)}</h3>
           </div>
-        ))
+        </>
       )}
     </div>
   );
